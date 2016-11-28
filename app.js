9649787d3c81473142c244d65483ba9532b982d1
brookwarren3 <brook.warren3@gmail.com>
2016-11-28 00:33:22 -0600

    created Array

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 
 
-
-
 function doTextAnalyze(string){
+  var words = $('#user-text').val().split(" ");
+  var text = [];
+  for (var i=0; i < words.length; i++) {
+  // only push this line if it contains a non whitespace character.
+    if (/\S/.test(words[i])) {
+      text.push($.trim(words[i]));
+    }
+  }
+  console.log(text.length);
+}
 
+
+function numberOfWords(string){
+  var numOfArray = doTextAnalyze(string);
+  console.log(numOfArray);
 }
 
 function finalRun(){
   $("form").submit(function( event ) {
+    event.preventDefault();
     var value = $('#user-text').val();
-    console.log(value);
+    numberOfWords(value);
   });
 }
 
